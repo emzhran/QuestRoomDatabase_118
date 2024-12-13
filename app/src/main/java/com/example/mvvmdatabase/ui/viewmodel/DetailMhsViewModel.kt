043ea9d858d47ba3d596f3cb30f961e2fd527b04
emzhran <zachran750@gmail.com>
2024-12-13 15:27:02 +0700

    menambahkan data class DetailUiState untuk status UI halaman

--- app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,6 +2,20 @@ package com.example.mvvmdatabase.ui.viewmodel
 
 import com.example.mvvmdatabase.data.entity.Mahasiswa
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+
 fun Mahasiswa.toDetailUiEvent():MahasiswaEvent{
     return MahasiswaEvent(
         nim = nim,
