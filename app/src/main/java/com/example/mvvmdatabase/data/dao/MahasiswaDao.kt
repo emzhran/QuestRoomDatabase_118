a93cb207c7e00d0db6223049b1b75b509a1e2688
emzhran <zachran750@gmail.com>
2024-12-13 14:37:05 +0700

    menambahkan fungsi getMahasiswa untuk mengambil data mahasiswa berdasarkan NIM, menambahkan fungsi deleteMahasiswa untuk menghapus data mahasiswa, dan menambahkan fungsi updateMahasiswa untuk memperbarui data mahasiswa

--- app/src/main/java/com/example/mvvmdatabase/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/mvvmdatabase/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.mvvmdatabase.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.mvvmdatabase.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -10,8 +12,17 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface MahasiswaDao {
     //fungsi get all data
-    @Query("select * from mahasiswa")
+    @Query("select * from mahasiswa ORDER BY nama ASC")
     fun getAllMahasiswa() : Flow<List<Mahasiswa>>
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa where nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
