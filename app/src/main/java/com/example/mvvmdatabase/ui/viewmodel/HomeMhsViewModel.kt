b7b966b9eda64656c2094200080b3a7b24ba71cc
emzhran <zachran750@gmail.com>
2024-12-13 15:17:23 +0700

    mengimplementasikan ViewModel untuk halaman home mahasiswa dan menggunakan repository untuk mengambil data mahasiswa

--- app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,7 +1,55 @@
 package com.example.mvvmdatabase.ui.viewmodel
 
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+import com.example.mvvmdatabase.repositroy.LocalRepositoryMhs
+import com.example.mvvmdatabase.repositroy.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+
+    val HomeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMahasiswa()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
