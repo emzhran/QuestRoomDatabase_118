a830c6f57c85073369f45a84362c931074f820fd
emzhran <zachran750@gmail.com>
2024-12-15 04:15:30 +0700

    menambahkan listmahasiswa untuk menampilkan daftar mahasiswa

--- app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/HomeMhsView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -21,6 +23,26 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+
+@Composable
+fun ListMahasiswa(
+    listMhs: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn(modifier = Modifier) {
+        items(
+            items = listMhs,
+            itemContent = {mhs ->
+                CardMhs(
+                    mhs = mhs,
+                    onClick = {onClick(mhs.nim)}
+                )
+            }
+        )
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
